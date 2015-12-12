df8e47abb211c142c0413f1e5f727f9aa477561d
stain88 <marcbaghdadi@gmail.com>
2015-12-12 11:54:20 +0000

    api element model

---
+++ api/models/element.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+var elementSchema = new mongoose.Schema({
+  name: String,
+  number: Number,
+  group: String,
+  position: Number,
+  small: String,
+  molar: Number,
+  electrons: [];
+})
+
+module.exports = mongoose.model('Element', elementSchema);
