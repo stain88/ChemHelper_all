272d0084f67c41d0a9e264aa9c3745aac624f4e6
stain88 <marcbaghdadi@gmail.com>
2015-12-12 11:37:18 +0000

    api index.js

--- api/index.js
+++ api/index.js
@@ -11,3 +11,23 @@ var app             = express(),
     config          = require(path.join(__dirname,'config','config')),
     secret          = config.secret;
 
+mongoose.connect(config.database);
+
+app.use(methodOverride(function(req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
+  };
+}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(morgan('dev'));
+app.use(cors());
+
+var routes = require(path.join(__dirname,'config', 'routes'));
+app.use('/api', routes);
+
+app.listen(3000);
