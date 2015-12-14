9d3d01a48fcaf016049b4fa1d089f96b79e90891
stain88 <marcbaghdadi@gmail.com>
2015-12-14 16:09:46 +0000

    api fact create

--- api/controllers/factsController.js
+++ api/controllers/factsController.js
@@ -1,3 +1,24 @@
 var Fact    = require('../models/fact'),
     Element = require('../models/element');
 
+function factsIndex(req, res) {
+  Fact.find({}, function(err, facts) {
+    if (err) return res.json({message: 'could not find any facts'});
+
+    res.json({facts: facts});
+  });
+};
+
+function factCreate(req, res) {
+  var fact = new Fact(req.body);
+  fact.save(function(err) {
+    if (err) return res.json({message: 'could not add fact: ' + err});
+
+    Element.findOne({_id: req.body.elem_id}, function(err, element) {
+      element.facts.push(fact);
+      element.save();
+    });
+    res.json({fact: fact});
+  });
+};
+
