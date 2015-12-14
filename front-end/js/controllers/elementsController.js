527960749cb32728c3912f3b3a98b4a6a93635f9
stain88 <marcbaghdadi@gmail.com>
2015-12-14 13:27:04 +0000

    all shells present

--- front-end/js/controllers/elementsController.js
+++ front-end/js/controllers/elementsController.js
@@ -27,11 +27,27 @@ function ElementsController(Element) {
     Element.get({id: element._id}, function(ele) {
       self.selectedElement = ele.element;
       self.selectedElement.protonSize = Math.sqrt(ele.element.number)+20;
+      self.selectedElement.rings = rings(ele.element.electrons);
+      console.log(self.selectedElement.rings);
     });
   }
 
   self.range = function(n) {
     return new Array(n);
+  }
+
+  function rings(arr) {
+    console.log(arr);
+    var count = 0;
+    for (var i=0;i<arr.length; i++) {
+      if (arr[i]!=="") count+=1;
+    }
+    console.log(count);
+    return count;
+  }
+
+  self.transform = function(n) {
+    return "rotate("+n+"deg)";
   }
 
   self.getElements();
