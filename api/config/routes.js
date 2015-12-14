a3867e9822b63ae20a03df5ab5172f6d8f1e1984
stain88 <marcbaghdadi@gmail.com>
2015-12-14 16:29:00 +0000

    Fact routes

--- api/config/routes.js
+++ api/config/routes.js
@@ -2,6 +2,7 @@ var express  = require('express'),
     router   = express.Router();
 
 var elementsController = require('../controllers/elementsController.js');
+var factsController = require('../controllers/factsController.js');
 
 router.route('/elements')
   .get(elementsController.getElements)
@@ -12,5 +13,15 @@ router.route('/elements/:id')
   .put(elementsController.updateElement)
   .patch(elementsController.updateElement)
   .delete(elementsController.deleteElement);
+
+router.route('/facts')
+  .get(factsController.factsIndex)
+  .post(factsController.factCreate);
+
+router.route('/facts/:id')
+  .get(factsController.factShow)
+  .put(factsController.factUpdate)
+  .patch(factsController.factUpdate)
+  .delete(factsController.factDelete);
 
 module.exports = router;
