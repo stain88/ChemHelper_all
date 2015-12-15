4f6dc51f1b16a1e885915c15df4f6469684130d2
stain88 <marcbaghdadi@gmail.com>
2015-12-15 14:55:45 +0000

    trying with put

--- api/index.js
+++ api/index.js
@@ -32,9 +32,8 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   if ('OPTIONS' == req.method) {
     res.send(200);
-  } else {
-    next();
   }
+  next();
 });
 
 var routes = require(path.join(__dirname,'config', 'routes'));
